42e31dd8ed2f8fcfebb7c8cc73820b67759e16a0
delfinblaksleymujica <delf.blaksley@hotmail.com>
2025-08-29 14:29:49 -0300

    Cambios en el navbar por delf

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/generals/Navbar";
+import Footer from "@/components/generals/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
