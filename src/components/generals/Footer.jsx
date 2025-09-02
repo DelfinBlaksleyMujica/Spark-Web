907d08ea8ccb9ad6a67d860bbaaa5683aa7ac5f0
delfinblaksleymujica <delf.blaksley@hotmail.com>
2025-09-02 13:42:26 -0300

    Footer terminado por miki

--- src/components/generals/Footer.jsx
+++ src/components/generals/Footer.jsx
@@ -11,14 +11,14 @@ const publicSans = Public_Sans({
 
 export default function Footer() {
   return (
-    <footer
-      className={`${publicSans.className} bg-[#FCD34D] text-zinc-900`}
-    >
+    <footer className={`${publicSans.className} bg-[#FCD34D] text-zinc-900`}>
       {/* TOP OF FOOTER */}
       <div className="grid grid-cols-1 md:grid-cols-12 gap-x-6 gap-y-10 pt-24 pb-6 md:pb-16 px-4 sm:px-6 md:px-0 text-center md:text-left">
         <section className="md:col-start-2 md:col-span-7">
-          <h2 className="font-regular leading-[0.95] tracking-tight text-balance
-                         text-5xl sm:text-6xl md:text-7xl lg:text-8xl">
+          <h2
+            className="font-regular leading-[0.95] tracking-tight text-balance
+                         text-5xl sm:text-6xl md:text-7xl lg:text-8xl"
+          >
             <span className="block">Transform teams,</span>
             <span className="block">Reduce turnover,</span>
             <span className="block font-semibold">Boost performance</span>
@@ -42,21 +42,37 @@ export default function Footer() {
           className="md:col-start-9 md:col-span-3 md:self-start"
         >
           <ul className="space-y-6 text-[25px] font-semibold leading-6 flex flex-col md:items-start sm:items-center ">
-            <li><Link href="/" className="hover:underline">Home</Link></li>
-            <li><Link href="/pricing" className="hover:underline">Pricing</Link></li>
-            <li><Link href="/about" className="hover:underline">About Us</Link></li>
-            <li><Link href="/blog" className="hover:underline">Blog</Link></li>
-            <li><Link href="/es" className="hover:underline">ES</Link></li>
+            <li>
+              <Link href="/" className="hover:underline">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/pricing" className="hover:underline">
+                Pricing
+              </Link>
+            </li>
+            <li>
+              <Link href="/about" className="hover:underline">
+                About Us
+              </Link>
+            </li>
+            <li>
+              <Link href="/blog" className="hover:underline">
+                Blog
+              </Link>
+            </li>
+            <li>
+              <Link href="/es" className="hover:underline">
+                ES
+              </Link>
+            </li>
           </ul>
         </nav>
       </div>
 
-
-
-
       {/* BOTTOM OF FOOTER */}
       <div className="grid grid-cols-1 md:grid-cols-12 gap-x-6 gap-y-8 pt-0 pb-12 md:pb-24 md:pt-2 px-4 sm:px-6 sm:py-12 md:px-0 text-center md:text-left ">
-
         {/* LEFT: Socials */}
         <section className="md:col-start-2 md:col-span-4 md:row-start-2 space-y-10 mx-auto md:mx-0">
           <div className="flex items-center gap-3 ">
@@ -93,19 +109,16 @@ export default function Footer() {
             </div>
 
             <p className="text-[14px] leading-6">
-              We are a comprehensive smart engagement platform designed to help companies
-              boost employee engagement, reduce turnover, and improve employee performance.
+              We are a comprehensive smart engagement platform designed to help
+              companies boost employee engagement, reduce turnover, and improve
+              employee performance.
             </p>
 
             <p className="text-[13px] mt-4 md:mt-auto">
               &copy; 2025 Spark. All Rights Reserved.
             </p>
           </div>
-
         </section>
-
-
-
 
         {/* RIGHT: Contact */}
         <section
@@ -128,11 +141,17 @@ export default function Footer() {
             Join Our Newsletter
           </h3>
           <p className="text-[20px] leading-6 max-w-sm ">
-            Sign up for our newsletter to enjoy free marketing tips, inspiration, and more
+            Sign up for our newsletter to enjoy free marketing tips,
+            inspiration, and more
           </p>
 
-          <form className="flex items-center gap-3 max-w-sm mx-auto md:mx-0" onSubmit={(e) => e.preventDefault()}>
-            <label htmlFor="email" className="sr-only">Email</label>
+          <form
+            className="flex items-center gap-3 max-w-sm mx-auto md:mx-0"
+            onSubmit={(e) => e.preventDefault()}
+          >
+            <label htmlFor="email" className="sr-only">
+              Email
+            </label>
             <input
               id="email"
               type="email"
@@ -150,11 +169,5 @@ export default function Footer() {
         </section>
       </div>
     </footer>
-  )
-};
-
-
-
-
-
-
+  );
+}
