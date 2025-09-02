ef46a1d14f83f3d3f9b4123f2639d5df6505f1ca
delfinblaksleymujica <delf.blaksley@hotmail.com>
2025-09-02 13:35:45 -0300

    Navbar responsive con animaciones terminado

--- src/components/generals/Navbar.jsx
+++ src/components/generals/Navbar.jsx
@@ -59,8 +59,8 @@ export default function Navbar() {
   const toggleMenu = () => setOpen((v) => !v);
 
   return (
-    <header className="h-[140px] w-full flex justify-center items-center fixed top-0">
-      <nav className="relative z-50 flex w-full max-w-[1440px] items-center justify-between gap-[40px] bg-[#000000] px-[18px] sm:px-[48px] md:px-[72px] lg:px-[118px] py-[24px] md:py-[40px] h-[140px]">
+    <header className="h-[100px] w-full flex justify-center items-center fixed top-0">
+      <nav className="relative z-50 flex w-full max-w-[1440px] items-center justify-between gap-[40px] bg-[#000000] px-[18px] sm:px-[48px] md:px-[72px] lg:px-[118px] py-[24px] md:py-[40px] h-full">
         {/* Logo */}
         <div className="h-full flex items-center">
           <Link href="/" aria-label="SparkClub - Home">
